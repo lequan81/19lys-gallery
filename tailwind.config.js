16945058475ece6e9631994d997c43d5771ca3b7
lequan81 <lequanruby@gmail.com>
2022-10-24 22:24:39 +0700

    update css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'Ubuntu': ['Ubuntu', 'sans-serif'],
+      'Mulish': ['Mulish', 'sans-serif'],
+      'Andika': ['Andika', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
